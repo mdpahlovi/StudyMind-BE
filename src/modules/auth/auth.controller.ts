05801c9d56f8d41ef19a128dbec31df0b34bc284
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2025-05-25 16:33:49 +0600

    Done Authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,28 +2,32 @@ import { User } from '@/database/schemas/user.schema';
 import { Public } from '@/decorators/public.decorator';
 import { LoginUserDto, RegisterUserDto } from '@/modules/auth/auth.dto';
 import { AuthService } from '@/modules/auth/auth.service';
-import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Response } from 'express';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Public()
     @Post('login')
+    @ApiOperation({ summary: 'Login' })
     async login(@Body() loginUserDto: LoginUserDto) {
         return await this.authService.login(loginUserDto);
     }
 
     @Public()
     @Post('register')
+    @ApiOperation({ summary: 'Register' })
     async register(@Body() registerUserDto: RegisterUserDto) {
         return await this.authService.register(registerUserDto);
     }
 
     @Public()
     @Get('google')
+    @ApiOperation({ summary: 'Google Login' })
     @UseGuards(AuthGuard('google'))
     async googleAuth(@Request() req) {
         /* Guard redirects */
@@ -31,6 +35,7 @@ export class AuthController {
 
     @Public()
     @Get('google/callback')
+    @ApiOperation({ summary: 'Google Login Callback' })
     @UseGuards(AuthGuard('google'))
     async googleAuthRedirect(@Request() req: { user: User }) {
         return await this.authService.loginOAuth(req.user);
@@ -38,6 +43,7 @@ export class AuthController {
 
     @Public()
     @Get('facebook')
+    @ApiOperation({ summary: 'Facebook Login' })
     @UseGuards(AuthGuard('facebook'))
     async facebookAuth(@Request() req) {
         /* Guard redirects */
@@ -45,6 +51,7 @@ export class AuthController {
 
     @Public()
     @Get('facebook/callback')
+    @ApiOperation({ summary: 'Facebook Login Callback' })
     @UseGuards(AuthGuard('facebook'))
     async facebookAuthRedirect(@Request() req: { user: User }) {
         return await this.authService.loginOAuth(req.user);
